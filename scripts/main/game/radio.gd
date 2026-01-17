0e44503c08b62615e8a93ae7dd26e754f9b138d6
IsaacCerejo <isaacerejo2@gmail.com>
2026-01-17 18:46:46 +0000

    feat: radio switch sound

--- scripts/main/game/radio.gd
+++ scripts/main/game/radio.gd
@@ -3,7 +3,11 @@ extends Button
 
 @export var musics: Array[SoundEffectSettings.SOUND_EFFECT_TYPE] = []
 
+@onready var switch_player: AudioStreamPlayer = $switch_player
+
 var _last_music = null
+
+var _switching: bool = false
 
 func _ready() -> void:
 	if musics.is_empty():
@@ -14,16 +18,30 @@ func _ready() -> void:
 	AudioManager.create_audio(first_song)
 
 func _pressed() -> void:
-
-	for music_type in musics:
-		AudioManager.stop_audio(music_type)
-
-	if musics.is_empty():
+	if musics.is_empty() or _switching:
 		return
+	
+	_switching = true
+	
+	# Stop currently playing musics
+	for music_type in musics:
+		AudioManager.fade_out_audio(music_type, 0.3)
 
+	# Pick next music
 	var next_music := _pick_new_music()
 	_last_music = next_music
+
+	var start_time = randf_range(1,38)
+	switch_player.play(start_time)
+
+	var tween := create_tween()
+	tween.tween_interval(0.6)
+	
+	await tween.finished
+	
+	switch_player.stop() 
 	AudioManager.create_audio(next_music)
+	_switching = false
 
 func _pick_new_music() -> SoundEffectSettings.SOUND_EFFECT_TYPE:
 	if musics.size() == 1:
