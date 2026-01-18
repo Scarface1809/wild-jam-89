9674021e5c47db10ef6f1d7a16f3ceea65ca45ff
IsaacCerejo <isaacerejo2@gmail.com>
2026-01-18 13:39:51 +0000

    feat: skip button

--- scripts/main/ui/shuffle.gd
+++ scripts/main/ui/shuffle.gd
@@ -1,18 +1,14 @@
 class_name Shuffle
-extends TextureButton
+extends Button
 
-@onready var label: Label = %Label
+
 
 func set_enabled(enabled: bool) -> void:
 	disabled = not enabled
 
 func _ready() -> void:
-	Global.game_state_changed.connect(sync_with_state)
 	Global.player_turn_started.connect(_on_player_turn_started)
 	Global.player_turn_ended.connect(_on_player_turn_ended)
-
-func sync_with_state(state: GameState, _action: Action) -> void:
-	label.text = str(state.deck.size())
 
 func _on_pressed() -> void:
 	Global.shuffle_request.emit()
